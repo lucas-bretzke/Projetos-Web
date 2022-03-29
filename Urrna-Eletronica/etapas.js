2a9ea84eba41dc99ef28ed52c9f9c957a40a7dd8
Lucas bretzke <lucasbretzke@gmail.com>
2022-03-29 06:49:30 -0300

    primeiro commit da calculadora

---
+++ Urrna-Eletronica/etapas.js
@@ -0,0 +1,56 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 2,
+        candidatos: [
+
+            {
+                numero: 66,
+                name: 'Lucas BKZ',
+                partido: 'PTDB',
+                fotos: [
+                    { url: '65.jpg', legenda: 'Vereador' }
+                ]
+            },
+
+
+            {
+                numero: 55,
+                name: 'Maria Bretzke',
+                partido: 'CTBS',
+                fotos: [
+                    { url: '65-2.jpg', legenda: 'Vereadora' }
+                ]
+            },
+        ]
+    },
+
+
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                name: 'Rodrigo Bretzke',
+                partido: 'PTDB',
+                vice: 'Lucas bretzke',
+                fotos: [
+                    { url: '99.jpg', legenda: 'Prefeito' },
+                    { url: '99-2.jpg', legenda: 'Vice Prefeito', small: true }
+                ]
+            },
+            {
+                numero: 77,
+                name: 'Soeli Coelho',
+                partido: 'SLA',
+                vice: 'Sabrina Bretzke',
+                fotos: [
+                    { url: '77.jpg', legenda: 'Prefeita' },
+                    { url: '77-2.jpg', legenda: 'Vice Prefeita', small: true }
+                ]
+            },
+        ]
+    }
+];
--- Validador-de-Formulario/script.js
+++ Validador-de-Formulario/script.js
@@ -1,56 +1,28 @@
-let B7validator = {
-    handleSubmit: (event) => {
-        event.preventDefault();
+const fields = document.querySelectorAll("[required]")
 
-        let send = true;
+// console.log(fields)
 
-        let inputs = form.querySelectorAll('.input');
+function customValidation(event) {
+    const field = event.target
 
-        for (let i = 0; i < inputs.length; i++) {
-            let input = inputs[i];
-            let check = B7validator.checkInput(input);
-            if (check !== true) {
-                send = false;
-                B7validator.showError(input, check); {
+    console.log(field.validity)
+    //trocar mensagem de required
+    field.setCustomValidity("Esse campo")
+}
 
-                }
-            }
-        }
-        send = false;
-        if (send) {
-            form.submit();
-        }
-    },
-    checkInput: (input) => {
-        let rules = input.getAttribute('data-rules');
-        if (rules !== null) {
-            rules = rules.split('|');
-            for (let k in rules);
-            let rDetails = rules[k].split('=');
-            switch (rDetails[0]) {
-                case 'required':
-                    if (input.value == '') {
-                        return 'Este campo é obrigatório.';
-                    }
-                    break;
-                case 'min':
-                    break;
-            }
-        }
+for (const field of fields) {
+    field.addEventListener("invalid", customValidation)
+}
 
-        return true;
-    },
 
-    showError: (input, error) => {
-        input.style.borderColor = '#FF0000';
 
-        let errorElement = document.createElement('div');   /*Cria um elemento (div,p,a etc...) */
-        errorElement.classList.add('error');
-        errorElement.innerHTML = error;
 
-        input.parentElement.insertBefore(errorElement, input.ElementSibling)  /*Adciona um elemento antes daquele campo */
-    }
-};
 
-let form = document.querySelector('.b7validator');
-form.addEventListener('.submit', B7validator.handleSubmit);
+
+document.querySelector("form")
+addEventListener("submit", event => {
+    console.log("Pode enviar o form")
+
+    //Nao vai enviar o formulario
+    event.preventDefault()
+})
