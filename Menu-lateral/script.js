2cfe4a0f6e77086f293f1e46b44b8aac663445e5
Lucas bretzke <lucasbretzke@gmail.com>
2022-03-31 13:26:39 -0300

    31/03  13:26

---
+++ Menu-lateral/script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById('myNav').style.width = '30%';
+    // document.getElementById('body').style.marginLeft = "30%"
+}
+
+function closeNav() {
+    document.getElementById('myNav').style.width = '0px';
+    // document.getElementById('body').style.marginLeft = "0px"
+
+
+}
