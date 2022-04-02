43f1b345a6928f29d5c0b74fc081ec303afffe51
Lucas bretzke <lucasbretzke@gmail.com>
2022-04-02 20:19:59 -0300

    nnn

---
+++ JogoDaVelha/script.js
@@ -0,0 +1,7 @@
+const celulas = document.querySelectorAll('.cell');
+const jogador_X = "X"
+const jogador_O = "O"
+
+document.addEventListener("click", (event) => {
+    console.log(event.target.id)
+});
