5fdb7ece04339e73dbd33d1b040b285cadee5c23
Lucas Bretzke <lucasbretzke@gmail.com>
2025-03-14 21:58:34 -0300

    commit 2

--- ListaDeCompras/script.js
+++ ListaDeCompras/script.js
@@ -1,25 +1,33 @@
+const itemInput = document.getElementById('item-input');
+const shoppingList = document.getElementById('shopping-list');
+
 function addItem() {
-    const itemInput = document.getElementById('item-input');
-    const shoppingList = document.getElementById('shopping-list');
     const itemText = itemInput.value.trim();
-
-
 
     if (itemText !== '') {
         const li = document.createElement('li');
         const p = document.createElement('p');
-        const btn = document.createElement('button');
-        const txtQuantity = document.createElement('span');
-        btn.textContent = 'X';
-        btn.onclick = function () {
-            shoppingList.removeChild(li);
+        const btnRemoveItem = document.createElement('button');
+        const txtQuantity = document.createElement('p');
+        const btnIncrement = document.createElement('button');
+        const btnDecrement = document.createElement('button');
+
+        btnIncrement.textContent = '+';
+        btnDecrement.textContent = '-';
+        btnRemoveItem.textContent = 'X';
+        txtQuantity.textContent = `Quantidade: ${1}`;
+
+        btnIncrement.onclick = () => txtQuantity.textContent = `Quantidade: ${parseInt(txtQuantity.textContent.split(': ')[1]) + 1}`;
+        btnDecrement.onclick = () => {
+            txtQuantity.textContent = `Quantidade: ${parseInt(txtQuantity.textContent.split(': ')[1]) - 1}`;
+            if (parseInt(txtQuantity.textContent.split(': ')[1]) <= 0) {
+                shoppingList.removeChild(li);
+            }
         };
+        btnRemoveItem.onclick = () => shoppingList.removeChild(li);
 
         p.textContent = itemText;
-        txtQuantity.textContent = `Quantidade: ${0}`;
-        li.appendChild(p);
-        li.appendChild(txtQuantity);
-        li.appendChild(btn);
+        li.append(p, txtQuantity, btnIncrement, btnDecrement, btnRemoveItem);
         shoppingList.appendChild(li);
         itemInput.value = '';
     }
