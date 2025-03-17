65fb48f9cc3ed525c0536b7952f4a530ea00626b
Lucas Bretzke <lucasbretzke@gmail.com>
2025-03-17 19:44:31 -0300

    ...

--- ListaDeCompras/script.js
+++ ListaDeCompras/script.js
@@ -1,5 +1,6 @@
 const itemInput = document.getElementById('item-input');
 const shoppingList = document.getElementById('shopping-list');
+const alreadyPurchased = document.getElementById('already-purchased');
 
 function addItem() {
     const itemText = itemInput.value.trim();
@@ -11,10 +12,12 @@ function addItem() {
         const txtQuantity = document.createElement('p');
         const btnIncrement = document.createElement('button');
         const btnDecrement = document.createElement('button');
+        const confirmationButton = document.createElement('button');
 
         btnIncrement.textContent = '+';
         btnDecrement.textContent = '-';
         btnRemoveItem.textContent = 'X';
+        confirmationButton.textContent = '✅';
         txtQuantity.textContent = `Quantidade: ${1}`;
 
         btnIncrement.onclick = () => txtQuantity.textContent = `Quantidade: ${parseInt(txtQuantity.textContent.split(': ')[1]) + 1}`;
@@ -27,7 +30,7 @@ function addItem() {
         btnRemoveItem.onclick = () => shoppingList.removeChild(li);
 
         p.textContent = itemText;
-        li.append(p, txtQuantity, btnIncrement, btnDecrement, btnRemoveItem);
+        li.append(p, txtQuantity, btnIncrement, btnDecrement, btnRemoveItem, confirmationButton);
         shoppingList.appendChild(li);
         itemInput.value = '';
     }
