d54c2dd907b6e1efd7acc3845fec1eab39be6847
Lucas Bretzke <lucasbretzke@gmail.com>
2025-03-20 21:52:32 -0300

    initiate project

--- my-angular-app/hello.world/src/app/app.component.ts
+++ my-angular-app/hello.world/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./components/nav/nav.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { ButtonComponent } from './components/button/button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- my-angular-app/hello.world/src/app/app.routes.ts
+++ my-angular-app/hello.world/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ButtonComponent } from './components/button/button.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'button', component: ButtonComponent },
+];
