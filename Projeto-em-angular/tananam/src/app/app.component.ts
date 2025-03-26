e81ff4f71b16516b93f88e7bfcdb81cb3fa4b314
Lucas Bretzke <lucasbretzke@gmail.com>
2025-03-25 21:58:38 -0300

    initiate e-commerce

---
+++ Projeto-em-angular/tananam/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
+import { NavComponent } from "./nav/nav.component";
+import { ItemComponent } from "./item/item.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FooterComponent, NavComponent, ItemComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'My E-commerce';
+
+  items = [
+    { name: 'Tênis Esportivo', promotion: true, isFavorite: true, price: 299.99, image: 'assets/1.png' },
+    { name: 'Tênis Casual', promotion: false, isFavorite: false, price: 199.99, image: 'assets/2.png' },
+    { name: 'Camiseta Esportiva', promotion: true, isFavorite: false, price: 99.99, image: 'assets/3.png' },
+    { name: 'Calça Jeans', promotion: false, isFavorite: true, price: 149.99, image: 'assets/4.png' },
+    { name: 'Jaqueta de Couro', promotion: true, isFavorite: false, price: 399.99, image: 'assets/5.png' },
+    { name: 'Relógio Digital', promotion: false, isFavorite: true, price: 249.99, image: 'assets/6.png' }
+  ];
+}
---
+++ Projeto-em-angular/tananam/src/app/item/item.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  standalone: true,
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent {
+  @Input() productName!: string;
+  @Input() productPrice!: number;
+  @Input() productImage!: string;
+  @Input() promotion!: boolean;
+  @Input() isFavorite!: boolean;
+}
