3dfee64e275c6c803ee3aafcde60c9aa9b77083c
Lucas Bretzke <lucasbretzke@gmail.com>
2025-03-31 19:35:53 -0300

    created nav

--- budget/src/components/nav/nav.component.ts
+++ budget/src/components/nav/nav.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
-
-}
+export class NavComponent {}
